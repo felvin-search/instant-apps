f2bee663372773fb26544fd5f19d8caa3204ce2a
rakaar <rka87338@gmail.com>
2021-06-16 00:40:19 +0530

    Made minor changes in jsonformatter app

--- src/SnippetApps/JSONFormatterApp.js
+++ src/SnippetApps/JSONFormatterApp.js
@@ -1,27 +1,34 @@
-import React, { useState } from 'react'
-import JSONPretty from 'react-json-pretty';
+import React, { useState } from "react";
+import JSONPretty from "react-json-pretty";
 
+function PrettifyJSON() {
+  const [jsonData, setJsonData] = useState({});
 
-export default function PrettifyJSON() {
-    const [jsonData, setJsonData] = useState({})
-
-    return(
-        <React.Fragment>
-            <textarea rows="10" cols="100" onChange={e => setJsonData(e.target.value)} />
-            <JSONPretty id="json-pretty" data={jsonData}></JSONPretty>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <textarea
+        rows="10"
+        cols="100"
+        placeholder="Enter your JSON to get formatted"
+        onChange={(e) => setJsonData(e.target.value)}
+      />
+      <JSONPretty id="json-pretty" data={jsonData}></JSONPretty>
+    </React.Fragment>
+  );
 }
 
+async function dummyFunction() {
+  // A function just to make sure component works
+  return [""];
+}
 
-// const PrettifyJSONApp = {
-//     name: "PrettifyJSON",
-//     id: "prettify_json",
-//     description: "Prettify JSON",
-//     logo: "",
-//     dataFetcher: null,
-//     renderer: CurrenyConvertor,
-// };
-  
-// export default PrettifyJSONApp;
-  
+const PrettifyJSONApp = {
+  name: "PrettifyJSON",
+  id: "prettify_json",
+  description: "Prettify JSON",
+  logo: "",
+  dataFetcher: dummyFunction,
+  renderer: PrettifyJSON,
+};
+
+export default PrettifyJSONApp;
