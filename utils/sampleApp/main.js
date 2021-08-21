812115f72149528eef9652e0958ac23d27837847
Harsh Gupta <mail@hargup.in>
2021-08-21 16:33:48 +0530

    Add script to create sample app (#11)
    Co-authored-by: Himanshu Mishra <himanshu@orkohunter.net>

---
+++ utils/sampleApp/main.js
@@ -0,0 +1,13 @@
+
+
+const SampleApp = {
+    name: "Sample App",
+    description:
+      "I am a sample app",
+    logo:
+      "",
+    dataFetcher: () => {return "Hello world"},
+    renderer: ({data}) => {return <div>{data}</div>},
+  };
+  
+  export default SampleApp;
