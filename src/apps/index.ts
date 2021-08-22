f52471dba851058b442c7f5e430ec191a8e5198f
Harsh Gupta <work@hargup.in>
2021-08-22 13:23:44 +0530

    Add JSON formatter app

--- src/apps/index.ts
+++ src/apps/index.ts
@@ -3,11 +3,12 @@
  */
 import dictionary from "./dictionary";
 import CurrencyConversionApp from "./CurrencyConversionApp"
+import JSONFormatterApp from "./JSONFormatterApp"
 // import timer from "./timer";
 
 /**
  * Note: Update me when adding a new app.
  */
-const availableApps = [dictionary, CurrencyConversionApp];
+const availableApps = [JSONFormatterApp, dictionary, CurrencyConversionApp];
 
 export default availableApps;
