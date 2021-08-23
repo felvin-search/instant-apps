2b8ac60551fc5eb1f92a1382c478849cd65b8919
Himanshu Mishra <himanshu@orkohunter.net>
2021-08-23 08:49:12 +0200

    load timer app
    Signed-off-by: Himanshu Mishra <himanshu@orkohunter.net>

--- src/apps/index.ts
+++ src/apps/index.ts
@@ -1,11 +1,11 @@
 /**
  * Note: Add a new import here when adding a new app.
  */
+import { InstantApp } from "./types";
 import dictionary from "./DictionaryApp";
 import CurrencyConversionApp from "./CurrencyConversionApp";
 import JSONFormatterApp from "./JSONFormatterApp";
-import { InstantApp } from "./types";
-// import timer from "./timer";
+import TimerApp from "./TimerApp";
 
 /**
  * Note: Update me when adding a new app.
@@ -14,6 +14,7 @@ const availableApps: Array<InstantApp> = [
   JSONFormatterApp,
   dictionary,
   CurrencyConversionApp,
+  TimerApp,
 ];
 
 export default availableApps;
