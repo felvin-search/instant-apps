0324f33ddffb1ea206a33437e0c86b235843ea32
Harsh Gupta <work@hargup.in>
2021-09-16 14:26:03 +0400

    Handle more trigger queries

--- src/apps/capitals.jsx
+++ src/apps/capitals.jsx
@@ -29,20 +29,26 @@ function territoryToCapital(territory) {
 
 const queryToTerritory = async ({ query }) => {
   const normalizedQuery = query.toLowerCase();
-  // I can use the standard function here probably
-  // Need to remove question marks
-  // The function should return normalized query without
-  // special characters
-  const triggers = ["capital of", "what is the capital of"];
-  if (normalizedQuery.startsWith("capital of")) {
-    const TerritoryName = normalizedQuery.replace("capital of", "").trim();
-    const CapitalName = territoryToCapital(TerritoryName);
-    if (CapitalName) {
-      return { territory: TerritoryName, capital: CapitalName };
-    } else {
-      return;
+  // Todo @Harsh: There is probably a smarter way to do this
+  // Todo @Harsh: Replace this with a standard utility function
+  const triggerQueries = [
+    "what is the capital of",
+    "what is capital of",
+    "capital of",
+    "capital",
+  ];
+  for (const triggerQuery of triggerQueries) {
+    if (normalizedQuery.startsWith(triggerQuery)) {
+      const TerritoryName = normalizedQuery.replace(triggerQuery, "").trim();
+      const CapitalName = territoryToCapital(TerritoryName);
+      if (CapitalName) {
+        return { territory: TerritoryName, capital: CapitalName };
+      } else {
+        return;
+      }
     }
   }
+
   return;
 };
 
