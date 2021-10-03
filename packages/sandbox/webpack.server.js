abc6c1b142d1993660e95378064c3727daf73bf7
Himanshu Mishra <himanshu@orkohunter.net>
2021-10-03 10:45:41 +0200

    allow direct css imports in apps like latex renderer
    Signed-off-by: Himanshu Mishra <himanshu@orkohunter.net>

--- packages/sandbox/webpack.server.js
+++ packages/sandbox/webpack.server.js
@@ -48,9 +48,16 @@ module.exports = {
           loader: "ts-loader",
         },
       },
+      // https://stackoverflow.com/a/61761653/4698026
       {
-        test: /\.css$/i,
-        use: ["file-loader"],
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"], // load project styles via style-loader
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["to-string-loader", "css-loader"], // use to-string-loader for 3rd party css
+        include: /node_modules/,
       },
     ],
   },
