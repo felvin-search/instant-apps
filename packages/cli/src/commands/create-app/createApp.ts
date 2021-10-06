f56fdad608525ae71f3dec6050b2634085e2347a
sahil-shubham <sahil.shubham2000@gmail.com>
2021-10-06 23:54:15 +0530

    create-app using felvin-cli done
    Co-authored-by: Himanshu Mishra <himanshu@orkohunter.net>

--- packages/cli/src/commands/create-app/createApp.ts
+++ packages/cli/src/commands/create-app/createApp.ts
@@ -5,19 +5,50 @@ import chalk from "chalk";
 import handlebars from "handlebars";
 import recursive from "recursive-readdir";
 import { exec } from "child_process";
+import { addNewAppDep, updateAppsArray } from "../../lib";
+import { camelCase, upperFirst } from "lodash";
+
+const PROJECT_HOME = resolve(__dirname, "../../../../..");
+const TEMPLATE_APP_PATH = join(
+  PROJECT_HOME,
+  "packages/cli/templates/default-app"
+);
+
+async function generateTemplate(directory, answers) {
+  try {
+    const files = await recursive(directory);
+    for (let file of files) {
+      if (file.endsWith(".hbs")) {
+        const template = await fs.readFile(file);
+        const compiled = handlebars.compile(template.toString());
+        const contents = compiled({
+          appId: answers.id,
+          appName:
+            answers.name !== ""
+              ? answers.name
+              : upperFirst(camelCase(answers.id)),
+          appDescription: answers.description,
+          triggerWords: answers.triggerWords
+            .split(",")
+            .map((item) => `"${item.trim()}"`),
+        });
+
+        await fs.writeFile(file, contents);
+        await fs.rename(file, file.replace(/\.hbs/, ""));
+      }
+    }
+  } catch (error) {
+    console.log(chalk.redBright("Error while generating template", error));
+  }
+}
 
 export async function main() {
-  const PROJECT_HOME = resolve(__dirname, "../../../../..");
-  const TEMPLATE_APP_PATH = join(
-    PROJECT_HOME,
-    "packages/cli/templates/default-app"
-  );
-
   const answers: Answers = await inquirer.prompt([
     {
       type: "input",
       name: "id",
-      message: "Enter an ID for the app [required]",
+      message:
+        "Enter an ID for the app (e.g. currency-convertor, dictionary)\n",
       validate: (value: any) => {
         if (!value) {
           return chalk.red("Please enter an ID for the app");
@@ -32,18 +63,12 @@ export async function main() {
     {
       type: "input",
       name: "name",
-      message: "Enter a name for the app [required]",
-      validate: (value: any) => {
-        if (!value) {
-          return chalk.red("Please enter a name for the app");
-        }
-        return true;
-      },
+      message: "Enter a name for the app [optional]\n",
     },
     {
       type: "input",
       name: "description",
-      message: "Enter a description for the app [required]",
+      message: "Enter a one line description for the app [required]\n",
       validate: (value: any) => {
         if (!value) {
           return chalk.red("Please enter a description for the app");
@@ -54,54 +79,43 @@ export async function main() {
     {
       type: "input",
       name: "triggerWords",
-      message: "Enter comma separated trigger words for the app [required]",
+      message:
+        "Enter trigger search words for the app (comma separated). [required] \n e.g. For a dictionary app: define, meaning\n",
       validate: (value: any) => {
         if (!value) {
-          return chalk.red("please enter trigger words for the app");
+          return chalk.red("Please enter trigger words for the app");
         }
         return true;
       },
     },
   ]);
 
-  async function generateTemplate(directory) {
-    try {
-      const files = await recursive(directory);
-      for (let file of files) {
-        if (file.endsWith(".hbs")) {
-          const template = await fs.readFile(file);
-          const compiled = handlebars.compile(template.toString());
-          const contents = compiled({
-            appId: answers.id,
-            appName: answers.name,
-            appDescription: answers.description,
-            triggerWords: answers.triggerWords
-              .split(",")
-              .map((item) => `"${item.trim()}"`),
-          });
-
-          await fs.writeFile(file, contents);
-          await fs.rename(file, file.replace(/\.hbs/, ""));
-        }
-      }
-    } catch (error) {
-      console.log(chalk.redBright("Error while generating template", error));
-    }
-  }
-
-  let FINAL_APP_PATH = join(PROJECT_HOME, `apps/${answers.id}`);
+  const FINAL_APP_PATH = join(PROJECT_HOME, `apps/${answers.id}`);
   if (await fs.pathExists(FINAL_APP_PATH)) {
     throw new Error("ID already exists, please try again with different id");
   }
 
   await fs.copy(TEMPLATE_APP_PATH, FINAL_APP_PATH);
+  await generateTemplate(FINAL_APP_PATH, answers);
+  await addNewAppDep(answers.id);
+  await updateAppsArray(answers.id);
 
-  await generateTemplate(FINAL_APP_PATH);
-
-  console.log(chalk.green(`New app created! Path: ${FINAL_APP_PATH}`));
-
-  console.log(chalk.blue("Installing dependencies"));
   exec("yarn install");
+
+  console.log(
+    chalk.green(
+      `Congratulations! You new app is created inside apps/${answers.id}`
+    )
+  );
+
+  console.log(
+    chalk.green("Start the development server using\n\nyarn start\n")
+  );
+  console.log(
+    `Here is a short guide on how your app works ${chalk.blue(
+      "https://docs.felvin.com/features/instant-apps/getting-started"
+    )}`
+  );
 }
 
 main().catch((error) => {
---
+++ packages/cli/src/lib/index.ts
@@ -0,0 +1,38 @@
+import { resolve, join } from "path";
+import fs from "fs-extra";
+import { camelCase, upperFirst } from "lodash";
+
+const felvinSearchAppsPath = resolve(__dirname, "../../../apps");
+const appsPackageJsonPath = join(felvinSearchAppsPath, "package.json");
+const appsIndexTsPath = join(felvinSearchAppsPath, "src/index.ts");
+
+const npmPackageForAppId = (appId: string) => {
+  return `@felvin-search-apps/${appId}`;
+};
+
+// Adds newly created app as a dependency in packages/apps/package.json
+export const addNewAppDep = async (appId: string) => {
+  const newNpmPackage = npmPackageForAppId(appId);
+  const appsPackageJson = await fs.readJson(appsPackageJsonPath);
+  appsPackageJson["dependencies"][newNpmPackage] = "^1.0.0";
+  await fs.writeJson(appsPackageJsonPath, appsPackageJson);
+};
+
+// Imports the newly created app in the sandbox
+export const updateAppsArray = async (appId: string) => {
+  const newNpmPackage = npmPackageForAppId(appId);
+
+  // e.g. converts currency-converter into CurrencyConverter
+  const appExportName = upperFirst(camelCase(appId));
+  let appsIndexTs = await fs.readFile(appsIndexTsPath, "utf-8");
+
+  const importLine = `import ${appExportName} from "${newNpmPackage}";\n`;
+  appsIndexTs = `${importLine}${appsIndexTs}`;
+
+  appsIndexTs = appsIndexTs.replace(
+    /const allApps = \[/gm,
+    `const allApps = [\n  ${appExportName},`
+  );
+
+  await fs.writeFile(appsIndexTsPath, appsIndexTs);
+};
