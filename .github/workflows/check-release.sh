a3b908382034003028bb2dacc325b5bf4f231c25
sahil-shubham <sahil.shubham2000@gmail.com>
2021-10-15 22:35:02 +0530

    excluded sandbox while publishing; committed version bump;

--- .github/workflows/check-release.sh
+++ .github/workflows/check-release.sh
@@ -4,9 +4,11 @@ git config user.name sahil-shubham
 git config user.email sahil.shubham2000@gmail.com
 
 # This checks if the latest commit message is of a release or not
-if [ "$(git describe --tags --abbrev=0)" != "$(git log -1 --pretty='%s')" ]; then
+if [ "Minor vesion bump;" != "$(git log -1 --pretty='%s')" ]; then
   yarn bump-version
-  git push --follow-tags
+  git add .
+  git commit -m "Minor version bump;"
+  git push
 else
   exit 1
 fi
