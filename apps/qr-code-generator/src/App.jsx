f8c9fb362b920630f76fd7db29e6ea34afa279ce
N-Shar-ma <bablinaneh@gmail.com>
2021-10-17 02:20:36 +0530

    Added revolving loader for QR code

--- apps/qr-code-generator/src/App.jsx
+++ apps/qr-code-generator/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef} from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { isTriggered } from "@felvin-search/core";
 
 //------------Styled Components-------------
@@ -24,6 +24,30 @@ const Textarea = styled.textarea`
   resize: none;
 `;
 
+const Img = styled.img`
+  display: ${props => props.hide ? "none" : "block"};
+`;
+
+const RotateInfinitely = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Loader = styled.div`
+  box-sizing: border-box;
+  height: 200px;
+  width: 200px;
+  border-radius: 50%;
+  border: 10px black solid;
+  border-top-color: transparent;
+  transform: rotate(0deg);
+  animation: ${RotateInfinitely} 700ms ease-in-out infinite;
+`;
+
 //=========================================
 
 // Your UI logic goes here.
@@ -31,19 +55,22 @@ const Textarea = styled.textarea`
 function Component({ data }) {
   const [content, setContent] = useState(data);
   const [qrContent, setQrContent] = useState(encodeURI(data));
+  const [loading, setLoading] = useState(true);
   const inputRef = useRef();
   const submitHandler = e => {
     e.preventDefault();
+    setLoading(true);
     setQrContent(encodeURI(inputRef.current.value));
   }
   return (
     <Container>
-        <h2>Generate QR Code</h2>
-        <Form onSubmit={submitHandler}>
-          <Textarea maxlength="900" ref={inputRef} onChange={() => setContent(inputRef.current.value)}>{content}</Textarea>
-          <button type="submit">Generate!</button>
-        </Form>
-      <img src={`https://api.qrserver.com/v1/create-qr-code/?data=${qrContent}&size=200x200`} alt={qrContent} />
+      <h2>Generate QR Code</h2>
+      <Form onSubmit={submitHandler}>
+        <Textarea maxlength="900" ref={inputRef} onChange={() => setContent(inputRef.current.value)}>{content}</Textarea>
+        <button type="submit">Generate!</button>
+      </Form>
+      {loading ? <Loader/> : null}
+      <Img hide={loading} onLoad={()=>setLoading(false)} src={`https://api.qrserver.com/v1/create-qr-code/?data=${qrContent}&size=200x200`} alt={qrContent} />
     </Container>
   );
 }
