dfb692c14dfe55aee6d543007884e4eac20fac25
rajivharlalka <rajivharlalka009@gmail.com>
2021-10-16 13:14:15 +0530

    select drop-down made

--- apps/crypto/src/App.jsx
+++ apps/crypto/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { isTriggered } from "@felvin-search/core";
 
@@ -10,6 +10,7 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 
 //=========================================
@@ -17,32 +18,66 @@ const Container = styled.div`
 // Your UI logic goes here.
 // `data` prop is exactly what is returned by queryToData.
 function Component({ data }) {
-  return <Container>You searched for: {data}</Container>;
+  let values = data.data;
+  let names = [];
+  values.forEach((e) => {
+    names.push({ name: e.name });
+  });
+  const [name, setName] = useState({ value: values[0].name });
+  const handleChange = (e) => {
+    setName({ value: e.target.value });
+  };
+  return (
+    <Container>
+      <div>
+        <select value={name.value} onChange={handleChange}>
+          {values.map((e) => (
+            <option key={e.name} name={e.name}>
+              {e.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </Container>
+  );
 }
 
 //=========================================
 
 // This where you can process the query and try to convert it into some meaningful data.
-const queryToData = ({ query }) => {
+const queryToData = async ({ query }) => {
   if (
-    !isTriggered(query, [
-      "crypto",
-      "cryptocurrency price",
-      "bitcoin price",
-      "ethereum price",
-      "dogecoin price",
-    ])
+    !isTriggered(
+      query,
+      [
+        "crypto",
+        "cryptocurrency price",
+        "bitcoin price",
+        "ethereum price",
+        "dogecoin price",
+      ],
+      { substringMatch: true }
+    )
   ) {
     return;
   }
 
+  const response = await fetch(
+    "https://data.messari.io/api/v1/assets?fields=id,name,symbol,metrics/market_data/price_usd,metrics/market_data/ohlcv_last_24_hour",
+    {
+      method: "GET",
+      redirect: "follow",
+    }
+  );
+  if (!response.ok) {
+    return;
+  }
+
+  const responseJson = await response.json();
+  return responseJson;
   // You can do any external API call or use any library here
   // to convert the search query into some meaningful data.
   // The data gets passed to the UI Component defined above.
-
-  const data = query.toUpperCase();
-
-  return data;
 };
 
 export { queryToData, Component };
