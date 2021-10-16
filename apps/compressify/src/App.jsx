ffcd519b471cdf0fc84b91067db302e21cb66ae5
rajivharlalka <rajivharlalka009@gmail.com>
2021-10-16 23:12:29 +0530

    compressify app started

---
+++ apps/compressify/src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import { isTriggered } from "@felvin-search/core";
+
+//------------Styled Components-------------
+// If you're unfamiliar with styled components
+// start here https://styled-components.com/docs/basics#getting-started
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+//=========================================
+
+// Your UI logic goes here.
+// `data` prop is exactly what is returned by queryToData.
+function Component({ data }) {
+  return (
+    <Container>
+      You searched for: {data}
+    </Container>
+  );
+}
+
+//=========================================
+
+// This where you can process the query and try to convert it into some meaningful data.
+const queryToData = ({ query }) => {
+  if (!isTriggered(query, [ "compress image","image compressor" ])) {
+    return;
+  }
+
+  // You can do any external API call or use any library here
+  // to convert the search query into some meaningful data.
+  // The data gets passed to the UI Component defined above.
+  
+  const data = query.toUpperCase();
+
+  return data;
+}
+
+export { queryToData, Component };
