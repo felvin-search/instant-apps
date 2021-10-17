4f8486dcc71566046f953b41431f17125bad91d2
N-Shar-ma <bablinaneh@gmail.com>
2021-10-17 21:10:56 +0530

    New App: Hello World In Different Languages

---
+++ apps/hello-world-in-different-languages/src/index.ts
@@ -0,0 +1,14 @@
+import { InstantApp } from "@felvin-search/core";
+import { Component, queryToData } from "./App";
+
+const App: InstantApp = {
+  id: "@felvin-search-apps/hello-world-in-different-languages",
+  name: "Hello World In Different Languages",
+  description: "Shows the introductory hello world code in many programming languages",
+  queryToData,
+  Component,
+  screenshotPath: "./files/screenshot.png",
+  exampleSearchQueries: ["hello world program", "code for hello world"],
+};
+
+export default App;
