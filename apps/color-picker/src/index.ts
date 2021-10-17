5d3bd3b025b8e082a0db2c3cf116664811902503
rajivharlalka <rajivharlalka009@gmail.com>
2021-10-17 17:09:53 +0530

    Screenshot and search Query added

--- apps/color-picker/src/index.ts
+++ apps/color-picker/src/index.ts
@@ -7,8 +7,8 @@ const App: InstantApp = {
   description: "Shows to change and display colors to pick",
   queryToData,
   Component,
-  // screenshotPath: "./files/screenshot.png",
-  // exampleSearchQueries: [],
+  screenshotPath: "./files/screenshot.png",
+  exampleSearchQueries: ["color picker"],
 };
 
 export default App;
