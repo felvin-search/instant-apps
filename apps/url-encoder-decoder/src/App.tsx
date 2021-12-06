b1a75ea1ecd3e494c63d01051b63ad8a2d1bf9a5
sahil-shubham <sahil.shubham2000@gmail.com>
2021-12-06 15:56:46 +0530

    inherit styles; remove unnecessary code

--- apps/url-encoder-decoder/src/App.tsx
+++ apps/url-encoder-decoder/src/App.tsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { isTriggered } from "@felvin-search/core";
 
@@ -6,7 +6,7 @@ import { isTriggered } from "@felvin-search/core";
 // If you're unfamiliar with styled components
 // start here https://styled-components.com/docs/basics#getting-started
 
-const TextBox = styled.input.attrs({type: 'text'})`
+const TextBox = styled.input.attrs({ type: "text" })`
   width: 100%;
 `;
 
@@ -17,18 +17,12 @@ const ButtonEnc = styled.button`
   color: white;
 `;
 
-const ButtonDec = styled.button`
+const ButtonDec = styled(ButtonEnc)`
   background: green;
-  width: 4.5rem;
-  margin: 0.8rem 1rem 0 0;
-  color: white;
 `;
 
-const ButtonCopy = styled.button`
+const ButtonCopy = styled(ButtonEnc)`
   background: blue;
-  width: 4.5rem;
-  margin: 0.8rem 1rem 0 0;
-  color: white;
 `;
 
 //=========================================
@@ -36,35 +30,42 @@ const ButtonCopy = styled.button`
 // Your UI logic goes here.
 // `data` prop is exactly what is returned by queryToData.
 function Component({ data }) {
-  const [url, seturl] = useState('');
+  const [url, seturl] = useState("");
 
-  const onChangeHandler = async (event) =>{
-    await seturl(event.target.value);
-  }
+  const onChangeHandler = (event) => {
+    seturl(event.target.value);
+  };
 
-  const handleEncode = async () => {
-    await seturl(encodeURIComponent(url));
-  }
+  const handleEncode = () => {
+    seturl(encodeURIComponent(url));
+  };
 
-  const handleDecode = async () =>{
-    await seturl(decodeURIComponent(url));
-  }
+  const handleDecode = () => {
+    seturl(decodeURIComponent(url));
+  };
 
-  const handleCopy = async (event) =>{
-    await navigator.clipboard.writeText(url).then(function() {
-      event.target.innerHTML = 'Copied';
-      setTimeout(()=>{
-        event.target.innerHTML = 'Copy';
-      }, 1000);
-    }, function(err) {
-      console.error('Async: Could not copy url: ', err);
-    });
-  }
+  const handleCopy = async (event) => {
+    await navigator.clipboard.writeText(url).then(
+      function () {
+        event.target.innerHTML = "Copied";
+        setTimeout(() => {
+          event.target.innerHTML = "Copy";
+        }, 1000);
+      },
+      function (err) {
+        console.error("Async: Could not copy url: ", err);
+      }
+    );
+  };
 
   return (
     <div>
       <div>
-        <TextBox type="text" placeholder="Enter url here..." value={url} onChange={onChangeHandler}/>  
+        <TextBox
+          placeholder="Enter url here..."
+          value={url}
+          onChange={onChangeHandler}
+        />
         <ButtonEnc onClick={handleEncode}>Encode</ButtonEnc>
         <ButtonCopy onClick={handleCopy}>Copy</ButtonCopy>
         <ButtonDec onClick={handleDecode}>Decode</ButtonDec>
@@ -77,7 +78,13 @@ function Component({ data }) {
 
 // This where you can process the query and try to convert it into some meaningful data.
 const queryToData = async ({ query }) => {
-  if (!isTriggered(query, [ "url encoder and decoder", "url decoder","url encoder","url encoder decoder","encoding url","decoding url","encoding and decoding url" ], { substringMatch: true })) {
+  if (
+    !isTriggered(
+      query,
+      ["url decoder", "url encoder", "encoding url", "decoding url"],
+      { substringMatch: true }
+    )
+  ) {
     return;
   }
 
@@ -88,6 +95,6 @@ const queryToData = async ({ query }) => {
   const data = query.toUpperCase();
 
   return data;
-}
+};
 
 export { queryToData, Component };
