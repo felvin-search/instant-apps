f62bb1d80047833060b74e5a579228ec1bf47064
sahil-shubham <sahil.shubham2000@gmail.com>
2021-12-06 22:55:03 +0530

    minor changes regarding styling of carousel; linter;
    Signed-off-by: sahil-shubham <sahil.shubham2000@gmail.com>

--- apps/instant-app-details/src/App.jsx
+++ apps/instant-app-details/src/App.jsx
@@ -2,7 +2,7 @@ import apps from "@felvin-search/apps";
 import React, { useState, useRef } from "react";
 import styled from "styled-components";
 import { isTriggered } from "@felvin-search/core";
-import "react-responsive-carousel/lib/styles/carousel.min.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
 //------------Styled Components-------------
@@ -14,17 +14,29 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  width: 100%;
 `;
 
 const Screenshot = styled.img`
   object-fit: contain;
-  height: 300px;
-  width:300px;
+
+  width: 50%;
+  aspect-ratio: 1/1;
   margin: 1rem;
 `;
 
 const Info = styled.div`
-  font-szie: 1rem;
+  font-size: 1.5rem;
+  margin: 1rem 0;
+`;
+
+const CarouselContainer = styled.div`
+  width: 100%;
+
+  @media (min-width: 900px) {
+    width: 600px;
+  }
 `;
 
 //=========================================
@@ -32,35 +44,47 @@ const Info = styled.div`
 // Your UI logic goes here.
 // `data` prop is exactly what is returned by queryToData.
 function Component({ data }) {
-
   function getAppById(id) {
-    const app = data.filter(app => app.id===id)
+    const app = data.filter((app) => app.id === id);
     return app[0];
   }
 
   function copyJSON() {
-    navigator.clipboard.writeText(JSON.stringify(data))
+    navigator.clipboard.writeText(JSON.stringify(data));
   }
 
-  const [currentApp, setCurrentApp] = useState(data[0])
-  const inputRef = useRef()
+  const [currentApp, setCurrentApp] = useState(data[0]);
+  const inputRef = useRef();
   return (
     <Container>
       {/* <button onClick={copyJSON}>Copy all JSON data to clipboard</button> */}
       {/* this button is not for prod */}
       <Info>Felvin currently has {data.length} instant apps!</Info>
-      <Carousel
-        width="300px"
-        showIndicators = {false}>
-      {data.map(currentApp => 
-      <Container key={currentApp.id}>
-        <h3>{currentApp.name}</h3>
-        <p>{currentApp.description}</p>
-        <p><a href={"https://felvin.com/search?q="+currentApp.exampleSearchQueries[0]}>{currentApp.exampleSearchQueries[0]}</a></p>
-        <Screenshot alt="screenshot showing the instant app" src={currentApp.screenshotPath}></Screenshot>
-      </Container>
-      )}
-      </Carousel>
+      {/* TODO: make the arrows visible without the need of hover on desktop */}
+      <CarouselContainer as={Carousel} showIndicators={false}>
+        {data.map((currentApp) => (
+          <div key={currentApp.id}>
+            <h3>{currentApp.name}</h3>
+            <p>{currentApp.description}</p>
+            <p>
+              <a
+                href={
+                  "https://felvin.com/search?q=" +
+                  currentApp.exampleSearchQueries[0]
+                }
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                {currentApp.exampleSearchQueries[0]}
+              </a>
+            </p>
+            <Screenshot
+              alt="screenshot showing the instant app"
+              src={currentApp.screenshotPath}
+            ></Screenshot>
+          </div>
+        ))}
+      </CarouselContainer>
     </Container>
   );
 }
@@ -69,7 +93,7 @@ function Component({ data }) {
 
 // This where you can process the query and try to convert it into some meaningful data.
 const queryToData = async ({ query }) => {
-  if (!isTriggered(query, [ "instant apps" ], { substringMatch: true })) {
+  if (!isTriggered(query, ["instant apps"], { substringMatch: true })) {
     return;
   }
 
@@ -77,13 +101,19 @@ const queryToData = async ({ query }) => {
   // to convert the search query into some meaningful data.
   // The data gets passed to the UI Component defined above.
 
-  const data = apps.map(({ id, name, description, screenshotPath, exampleSearchQueries }) => {
-    const details = { id, name, description, exampleSearchQueries }
-    details.screenshotPath = "https://raw.githubusercontent.com/felvin-search/instant-apps/master/apps/" + id.split('/')[1] + "/src" + screenshotPath.substring(1)
-    return details
-  })
+  const data = apps.map(
+    ({ id, name, description, screenshotPath, exampleSearchQueries }) => {
+      const details = { id, name, description, exampleSearchQueries };
+      details.screenshotPath =
+        "https://raw.githubusercontent.com/felvin-search/instant-apps/master/apps/" +
+        id.split("/")[1] +
+        "/src" +
+        screenshotPath.substring(1);
+      return details;
+    }
+  );
 
   return data;
-}
+};
 
 export { queryToData, Component };
