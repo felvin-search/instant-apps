c750927d6ede5bca24a374f7ffbf8545a775eaa6
sahil-shubham <sahil.shubham2000@gmail.com>
2021-12-07 22:49:23 +0530

    fixed reference code app query to data func and rendering;
    Signed-off-by: sahil-shubham <sahil.shubham2000@gmail.com>

--- apps/code-reference/src/App.jsx
+++ apps/code-reference/src/App.jsx
@@ -1,20 +1,6 @@
 import React from "react";
-import styled from "styled-components";
-import { isTriggered } from "@felvin-search/core";
 import code from "../sample-data/data.json";
 import Editor from "@monaco-editor/react";
-
-const languages = [
-  "c++",
-  "js",
-  "javascript",
-  "python",
-  "py",
-  "bash",
-  "golang",
-  "shell",
-  "haskell",
-];
 
 //------------Styled Components-------------
 // If you're unfamiliar with styled components
@@ -24,15 +10,15 @@ const languages = [
 
 // Your UI logic goes here.
 // `data` prop is exactly what is returned by queryToData.
-function Component([algorithm,language]) {
+function Component(props) {
   return (
     <div>
       <Editor
         height="50vh"
         width="60vw"
         theme="vs-dark"
-        defaultLanguage={language}
-        defaultValue={code[algorithm][language]}
+        defaultLanguage={props.data.language}
+        defaultValue={code[props.data.algorithm][props.data.language]}
       />
     </div>
   );
@@ -54,24 +40,13 @@ const queryToData = async ({ query }) => {
 
   const [algorithm, language] = query.split(" in ");
 
-  if (Object.keys(code).includes(algorithm) && languages.includes(language)) {
-    return [algorithm, language];
+  if (
+    Object.keys(code).includes(algorithm) &&
+    Object.keys(code[algorithm]).includes(language)
+  ) {
+    return { algorithm, language };
   }
   return;
-
-  if (
-    !isTriggered(query, ["bfs in js", "dfs in c++", "binary search in python"])
-  ) {
-    return;
-  }
-
-  // You can do any external API call or use any library here
-  // to convert the search query into some meaningful data.
-  // The data gets passed to the UI Component defined above.
-
-  const data = query.toUpperCase();
-
-  return data;
 };
 
 export { queryToData, Component };
