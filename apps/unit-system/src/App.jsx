62b891e9a0d99ed47d4ba965f15d18495d2e0da6
Shailesh Nanisetty <shaileshnanisetty05@gmail.com>
2021-12-08 23:36:45 +0530

    adding a new app named - unit-system

--- apps/unit-system/src/App.jsx
+++ apps/unit-system/src/App.jsx
@@ -1,44 +1,136 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { isTriggered } from "@felvin-search/core";
+import unitstable from "./diffunits.json";
 
 //------------Styled Components-------------
 // If you're unfamiliar with styled components
 // start here https://styled-components.com/docs/basics#getting-started
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const OutputString = styled.div`
+  font-size: medium;
+  padding: 0 0 1.25rem 0;
+  text-align: center;
 `;
 
+const ColContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 //=========================================
 
 // Your UI logic goes here.
 // `data` prop is exactly what is returned by queryToData.
+
 function Component({ data }) {
-  return (
-    <Container>
-      You searched for: {data}
-    </Container>
-  );
+    const [inputlength, setInputlength] = useState(data.inputlength);
+    const [outputlength, setOutputlength] = useState(data.outputlength);
+
+    const [inputValue, setInputValue] = useState(data.amount);
+    const [outputValue, setOutputValue] = useState(0);
+    const converter = () => {
+        const metres = unitstable[inputlength]["metre"] * inputValue;
+        setOutputValue(unitstable["metre"][outputlength] * metres);
+    };
+    useEffect(() => {
+        converter();
+    }, []);
+
+    return (
+        <ColContainer>
+            <OutputString>
+                {inputValue} {inputlength} equals{" "}
+                <b>
+                    {outputValue} {outputlength}
+                </b>
+            </OutputString>
+        </ColContainer>
+    );
 }
+
+const isValidLengthUnit = (str) => {
+    const units_length = [
+        "metre",
+        "centimetre",
+        "millimetre",
+        "kilometre",
+        "inch",
+        "feet",
+        "nanometre",
+        "yard"
+    ];
+    if (lengthUnits.includes(str)) return true;
+};
+
+const mapUnits = (str) => {
+    if (str == "metre" || str =="m") {
+        return "metre";
+    }
+    else if (str == "centimetre" || str == "cm") {
+        return "centimetre";
+    }
+    else if (str == "millimetre" || str == "mm") {
+        return "millimetre";
+    }
+    else if (str == "kilometre" || str == "km") {
+        return "kilometre";
+    }
+    else if (str == "inch" || str == "in") {
+        return "inch";
+    }
+    else if (str == "feet" || str == "ft") {
+        return "feet";
+    }
+    else if (str == "nanometre" || str == "nm") {
+        return "nanometre";
+    }
+    else if (str == "yard" || str == "yd") {
+        return "yard";
+    }
+    else {
+        return str;
+    }
+}
+
+const parseConversionString = (query) => {
+    const normalizedQuery = query.toLowerCase();
+    if (normalizedQuery.includes("convert") && normalizedQuery.includes("to")) {
+        const tokens = normalizedQuery
+            .split(/([0-9.]+)/)
+            .join(" ")
+            .split(" ")
+            .filter((s) => s !== "");
+        if (tokens.length === 5) {
+            try {
+                const amount = parseFloat(tokens[1]);
+                return { amount, from: tokens[2], to: tokens[4] };
+            } catch {
+                return null;
+            }
+        }
+        return null;
+    }
+};
 
 //=========================================
 
 // This where you can process the query and try to convert it into some meaningful data.
-const queryToData = async ({ query }) => {
-  if (!isTriggered(query, [ "convert" ])) {
-    return;
-  }
-
-  // You can do any external API call or use any library here
-  // to convert the search query into some meaningful data.
-  // The data gets passed to the UI Component defined above.
-
-  const data = query.toUpperCase();
-
-  return data;
+async function queryToData({ query }) {
+    try {
+        let { amount, from, to } = parseConversionString(query);
+        from = mapUnits(from);
+        to = mapUnits(to);
+        if (isValidLengthUnit(from) && isValidLengthUnit(to)) {
+            return {
+                amount,
+                inputlength: from,
+                outputlength: to,
+            };
+        } else {
+            return null;
+        }
+    } catch {
+        return null;
+    }
 }
 
 export { queryToData, Component };
--- apps/unit-system/src/index.ts
+++ apps/unit-system/src/index.ts
@@ -3,12 +3,12 @@ import { Component, queryToData } from "./App";
 
 const App: InstantApp = {
   id: "@felvin-search-apps/unit-system",
-  name: "Length-Convertor",
-  description: "This app converts length measurement from one unit to another.",
+  name: "Converting given length to metre(SI unit)",
+  description: "This is a app which gives you the measurement in S.I unit metre from any kind of unit.",
   queryToData,
   Component,
-  // screenshotPath: "./files/screenshot.png",
-  // exampleSearchQueries: [],
+  screenshotPath: "./files/screenshot.png",
+  exampleSearchQueries: ["convert 32 inch to metres"],
 };
 
 export default App;
