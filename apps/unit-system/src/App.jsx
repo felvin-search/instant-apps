ff490a9fa2d04805e3bf8b2875a4471f13c3e912
sahil-shubham <sahil.shubham2000@gmail.com>
2021-12-11 15:56:25 +0530

    removed uneccesary change; updated screenshot; linter;
    Signed-off-by: sahil-shubham <sahil.shubham2000@gmail.com>

--- apps/unit-system/src/App.jsx
+++ apps/unit-system/src/App.jsx
@@ -15,7 +15,7 @@ const OutputString = styled.div`
 const ColContainer = styled.div`
   display: flex;
   flex-direction: column;
-`
+`;
 //=========================================
 
 // Your UI logic goes here.
@@ -27,86 +27,78 @@ function Component({ data }) {
   const [inputValue, setInputValue] = useState(data.amount);
   const [outputValue, setOutputValue] = useState(0);
   const converter = () => {
-      const metres = conversionsTable[inputlength]["metre"] * inputValue;
-      setOutputValue(conversionsTable["metre"][outputlength] * metres);
+    const metres = conversionsTable[inputlength]["metre"] * inputValue;
+    setOutputValue(conversionsTable["metre"][outputlength] * metres);
   };
   useEffect(() => {
-      converter();
+    converter();
   }, []);
 
   return (
-      <ColContainer>
-          <OutputString>
-              {inputValue} {inputlength} equals{" "}
-              <b>
-                  {outputValue} {outputlength}
-              </b>
-          </OutputString>
-      </ColContainer>
+    <ColContainer>
+      <OutputString>
+        {inputValue} {inputlength} equals{" "}
+        <b>
+          {outputValue} {outputlength}
+        </b>
+      </OutputString>
+    </ColContainer>
   );
 }
 
 const isValidLengthUnit = (str) => {
   const units_length = [
-      "metre",
-      "centimetre",
-      "millimetre",
-      "kilometre",
-      "inch",
-      "feet",
-      "nanometre",
-      "yard"
+    "metre",
+    "centimetre",
+    "millimetre",
+    "kilometre",
+    "inch",
+    "feet",
+    "nanometre",
+    "yard",
   ];
   if (units_length.includes(str)) return true;
 };
 
 const mapUnits = (str) => {
-  if (str == "metre" || str =="m") {
-      return "metre";
+  if (str == "metre" || str == "m") {
+    return "metre";
+  } else if (str == "centimetre" || str == "cm") {
+    return "centimetre";
+  } else if (str == "millimetre" || str == "mm") {
+    return "millimetre";
+  } else if (str == "kilometre" || str == "km") {
+    return "kilometre";
+  } else if (str == "inch" || str == "in") {
+    return "inch";
+  } else if (str == "feet" || str == "ft") {
+    return "feet";
+  } else if (str == "nanometre" || str == "nm") {
+    return "nanometre";
+  } else if (str == "yard" || str == "yd") {
+    return "yard";
+  } else {
+    return str;
   }
-  else if (str == "centimetre" || str == "cm") {
-      return "centimetre";
-  }
-  else if (str == "millimetre" || str == "mm") {
-      return "millimetre";
-  }
-  else if (str == "kilometre" || str == "km") {
-      return "kilometre";
-  }
-  else if (str == "inch" || str == "in") {
-      return "inch";
-  }
-  else if (str == "feet" || str == "ft") {
-      return "feet";
-  }
-  else if (str == "nanometre" || str == "nm") {
-      return "nanometre";
-  }
-  else if (str == "yard" || str == "yd") {
-      return "yard";
-  }
-  else {
-      return str;
-  }
-}
+};
 
 const parseConversionString = (query) => {
   const normalizedQuery = query.toLowerCase();
   if (normalizedQuery.includes("convert") && normalizedQuery.includes("to")) {
-      const tokens = normalizedQuery
-          .split(/([0-9.]+)/)
-          .join(" ")
-          .split(" ")
-          .filter((s) => s !== "");
-      if (tokens.length === 5) {
-          try {
-              const amount = parseFloat(tokens[1]);
-              return { amount, from: tokens[2], to: tokens[4] };
-          } catch {
-              return null;
-          }
+    const tokens = normalizedQuery
+      .split(/([0-9.]+)/)
+      .join(" ")
+      .split(" ")
+      .filter((s) => s !== "");
+    if (tokens.length === 5) {
+      try {
+        const amount = parseFloat(tokens[1]);
+        return { amount, from: tokens[2], to: tokens[4] };
+      } catch {
+        return null;
       }
-      return null;
+    }
+    return null;
   }
 };
 
@@ -115,20 +107,20 @@ const parseConversionString = (query) => {
 // This where you can process the query and try to convert it into some meaningful data.
 async function queryToData({ query }) {
   try {
-      let { amount, from, to } = parseConversionString(query);
-      from = mapUnits(from);
-      to = mapUnits(to);
-      if (isValidLengthUnit(from) && isValidLengthUnit(to)) {
-          return {
-              amount,
-              inputlength: from,
-              outputlength: to,
-          };
-      } else {
-          return null;
-      }
-  } catch {
+    let { amount, from, to } = parseConversionString(query);
+    from = mapUnits(from);
+    to = mapUnits(to);
+    if (isValidLengthUnit(from) && isValidLengthUnit(to)) {
+      return {
+        amount,
+        inputlength: from,
+        outputlength: to,
+      };
+    } else {
       return null;
+    }
+  } catch {
+    return null;
   }
 }
 
--- apps/unit-system/src/index.ts
+++ apps/unit-system/src/index.ts
@@ -4,7 +4,8 @@ import { Component, queryToData } from "./App";
 const App: InstantApp = {
   id: "@felvin-search-apps/unit-system",
   name: "Converting given length to metre(SI unit)",
-  description: "This is a app which gives you the measurement in S.I unit metre from any kind of unit.",
+  description:
+    "This is a app which gives you the measurement in S.I unit metre from any kind of unit.",
   queryToData,
   Component,
   screenshotPath: "./files/screenshot.png",
