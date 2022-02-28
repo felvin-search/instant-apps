5972430c0c640414fc9fbe98989915fcb1358ea8
RaaVanansri <vigneshseetharam@outlook.com>
2022-02-28 22:01:34 +0530

    Added pacman game

---
+++ apps/pacman/src/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+import { isTriggered } from "@felvin-search/core";
+import Pacman from 'react-pacman';
+
+
+//------------Styled Components-------------
+// If you're unfamiliar with styled components
+// start here https://styled-components.com/docs/basics#getting-started
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+//=========================================
+
+// Your UI logic goes here.
+// `data` prop is exactly what is returned by queryToData.
+function Component({ data }) {
+  return (
+    <Container>
+      <Pacman />
+    </Container>
+  );
+}
+
+//=========================================
+
+// This where you can process the query and try to convert it into some meaningful data.
+const queryToData = async ({ query }) => {
+  if (!isTriggered(query, [ "pacman","pac man","pac-man" ])) {
+    return;
+  }
+
+  // You can do any external API call or use any library here
+  // to convert the search query into some meaningful data.
+  // The data gets passed to the UI Component defined above.
+
+  const data = query.toUpperCase();
+
+  return data;
+}
+
+export { queryToData, Component };
---
+++ apps/pacman/src/index.ts
@@ -0,0 +1,14 @@
+import { InstantApp } from "@felvin-search/core";
+import { Component, queryToData } from "./App";
+
+const App: InstantApp = {
+  id: "@felvin-community/pacman",
+  name: "pacman",
+  description: "pacman game intergration with felvin",
+  queryToData,
+  Component,
+  // screenshotPath: "./files/screenshot.png",
+  // exampleSearchQueries: [],
+};
+
+export default App;
