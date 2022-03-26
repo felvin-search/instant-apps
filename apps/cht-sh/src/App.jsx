9394c0947a1072ef32a9e9b4452b2b9afc79f4e9
Harsh Gupta <work@hargup.in>
2022-03-25 18:35:18 -0700

    don't show cheat-sh when there is no result (#342)
    * don't show cheat-sh when there is no result
    
    * remove extaneous console logs

--- apps/cht-sh/src/App.jsx
+++ apps/cht-sh/src/App.jsx
@@ -6,35 +6,6 @@ import { isTriggered } from "@felvin-search/core";
 // If you're unfamiliar with styled components
 // start here https://styled-components.com/docs/basics#getting-started
 
-/*
-body {
-    background: black;
-    color: #bbbbbb;
-}
-.pre,
-pre {
-/*    font-family: source_code_proregular; */
-
-/*
-font-family: Courier New,Courier,Lucida Sans Typewriter,Lucida Typewriter,monospace;
-font-size: 70%;
-*/
-
-/*font-family: Lucida Console,Lucida Sans Typewriter,monaco,Bitstream Vera Sans Mono,monospace; */
-/*Droid Sans Mono*/
-// font-family: "DejaVu Sans Mono", Menlo, "Lucida Sans Typewriter", "Lucida Console", monaco, "Bitstream Vera Sans Mono", monospace;
-// /*font-family: bitstream_vera_sans_monoroman;*/
-// font-size: 75%;
-// }
-
-// input[type="text"]{
-//     border: none;
-//     background: transparent;
-//     color: #bbbbbb;
-// }
-
-
-// */
 const Container = styled.div`
   background: black;
   color: #bbbbbb;
@@ -52,7 +23,6 @@ const Container = styled.div`
 // Your UI logic goes here.
 // `data` prop is exactly what is returned by queryToData.
 function Component({ data }) {
-  // console.log(data.keys())
   console.log(data)
   const markup = {"__html": data["html"]}
   return (
@@ -63,15 +33,11 @@ function Component({ data }) {
 }
 
 const getSnippet = async (query) => {
-  // return "Yolo"
   const response = await fetch(`https://cheat-sh.felvin.com/${query}`);
   if(response.ok){
-    console.log("Got output from cht.sh")
-
     const data = await response.text()
     return data;
   } else {
-    console.log("Didn't get output from cht.sh")
     return;
   }
 
@@ -81,9 +47,8 @@ const getSnippet = async (query) => {
 
 // This where you can process the query and try to convert it into some meaningful data.
 const queryToData = async ({ query }) => {
-  console.log("Inside Cheat.sh app")
   const html = await getSnippet(query)
-  if(!html.includes("Unknown topic")){
+  if(!html.includes("Unknown topic") && !html.includes("404 NOT FOUND")){
     return {html};
   } else {
     return;
