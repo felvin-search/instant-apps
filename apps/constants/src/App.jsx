8f4a3987c97c281e2bfcc3b37a8a8f43457c7e8b
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-04-15 00:18:26 +0530

    Added Constants instant app(#352)

---
+++ apps/constants/src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { isTriggered } from "@felvin-search/core";
+import { constants } from "./utils/Constants";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 3rem;
+  align-items: center;
+  width: 50vw;
+  min-height: 10rem;
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+`;
+const Symbols = styled.div`
+  font-size: 2rem;
+`;
+const Gap=styled.div`
+  height: 1rem;
+
+`
+
+function Component({ data }) {
+  
+
+  return (
+    <Container>
+      <Symbols>
+        Symbol : <b dangerouslySetInnerHTML={{ __html: data[0].symbol }}></b>
+      </Symbols>
+      <Gap/>
+      <div>Name: {data[0].name}</div>
+      <Gap/>
+      <div>Value : {data[0].value}</div>
+      <Gap/>
+    { data[0].unit && <div>Unit : {data[0].unit}</div>}
+    </Container>
+  );
+}
+
+
+const queryToData = async ({ query }) => {
+  query = query.toLowerCase();
+  //use to extract keywords from the strings
+  //let R = /(\w|\s)*\w(?=")|\w+/g;
+  let filteredQuery = query.split(' ').filter(subStr=>subStr.length!==0);
+  const data = constants.filter((key) => {
+    return key.name.toLowerCase().includes(filteredQuery[0]);
+  });
+  if (data.length === 0) {
+    return;
+  }
+
+  return data;
+};
+
+export { queryToData, Component };
---
+++ apps/constants/src/index.ts
@@ -0,0 +1,14 @@
+import { InstantApp } from "@felvin-search/core";
+import { Component, queryToData } from "./App";
+
+const App: InstantApp = {
+  id: "@felvin-community/constants",
+  name: "constants",
+  description: "Provides Value of Mathematical Constants",
+  queryToData,
+  Component,
+  screenshotPath: "./files/screenshot.png",
+  exampleSearchQueries: ['proton mass','pi','rydberg constant'],
+};
+
+export default App;
