5b59e489237002875aa751698faa3663d2feccc8
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-04-20 20:38:14 +0530

    initial fix

--- apps/code-reference/src/App.jsx
+++ apps/code-reference/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
 import axios from "axios";
 import styled from "styled-components";
 import _ from "lodash";
@@ -10,6 +10,16 @@ import _ from "lodash";
 // start here https://styled-components.com/docs/basics#getting-started
 const Source = styled.span`
   font-size: 1rem;
+  font-style: italic;
+  font-weight: 400;
+  color: #878787;
+`;
+
+const SourceLink = styled.a`
+  text-decoration: none;
+  color: #878787;
+  margin-left: 4px;
+  font-weight: 700;
 `;
 
 const CodeBlock = styled.div`
@@ -17,7 +27,8 @@ const CodeBlock = styled.div`
   width: clamp(300px, 60vw, 900px);
   overflow-y: auto;
   font-size: 1rem;
-`
+  
+`;
 
 const Container = styled.div`
   display: flex;
@@ -28,6 +39,7 @@ const Container = styled.div`
 
 const Clipboard = styled.button`
   margin: 0;
+ 
 `;
 
 //=========================================
@@ -40,17 +52,19 @@ function Component(props) {
     <div>
       <p>{_.startCase(props.data.algorithm)}</p>
       <CodeBlock>
-        <SyntaxHighlighter 
-          language={props.data.language} 
-          style={a11yDark}
+        <SyntaxHighlighter
+          language={props.data.language}
+          // style={oneLight}
           wrapLongLines={true}
         >
           {JSON.parse(props.data.code)}
         </SyntaxHighlighter>
+        
       </CodeBlock>
       <Container>
         <Source>
-          Source : <a href={props.data.source}>{props.data.name}</a>
+          Source :
+          <SourceLink href={props.data.source}>{props.data.name}</SourceLink>
         </Source>
         <Clipboard
           onClick={() => {
@@ -73,12 +87,12 @@ const queryToData = async ({ query }) => {
   //  -> Language - Mostly Single Word
   const languageMap = {
     js: "javascript",
-    cpp: "c++"
-  }
+    cpp: "c++",
+  };
   query = query.toLowerCase();
-  Object.keys(languageMap).forEach(shortForm => {
+  Object.keys(languageMap).forEach((shortForm) => {
     query = query.replace(shortForm, languageMap[shortForm]);
-  })
+  });
   const searchQuery = query;
   const value = await axios.get(
     "https://felvin-service.herokuapp.com/api/code",
