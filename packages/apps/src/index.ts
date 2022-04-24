2c4a98d026082ba8ac5dd364a5a77a134015f5ef
rajivharlalka <rajivharlalka009@gmail.com>
2022-04-24 12:28:22 +0530

    ascii art instant app added

--- packages/apps/src/index.ts
+++ packages/apps/src/index.ts
@@ -1,3 +1,4 @@
+import AsciiArt from "@felvin-community/ascii-art";
 import Constants from "@felvin-community/constants";
 import Bmiconverter from "@felvin-community/bmiconverter";
 import JsonToYaml from "@felvin-community/json-to-yaml";
@@ -68,6 +69,7 @@ import CourierTracker from "@felvin-community/courier-tracker";
 import YamlToJson from "@felvin-community/yaml-to-json";
 
 const allApps = [
+  AsciiArt,
   Constants,
   Bmiconverter,
   JsonToYaml,
