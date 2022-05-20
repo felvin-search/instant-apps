c96e55ed5197c74aa735a6b259fb7674ef3b5693
Abhishek K M <67158080+Sync271@users.noreply.github.com>
2022-05-21 00:00:39 +0530

    add example queries

--- apps/corrupt-pdf/src/index.ts
+++ apps/corrupt-pdf/src/index.ts
@@ -8,7 +8,7 @@ const App: InstantApp = {
   queryToData,
   Component,
   screenshotPath: "./files/screenshot.png",
-  // exampleSearchQueries: [],
+  exampleSearchQueries: ["corrupt pdf file", "corrupt pdf", "pdf corrupt"],
 };
 
 export default App;
